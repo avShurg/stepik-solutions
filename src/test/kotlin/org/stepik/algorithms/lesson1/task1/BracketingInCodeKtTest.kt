d2ab74ac3e2f3d34c6be5a694e008857ec794faa
ShurganovAV <shurganov.av94@gmail.com>
2019-11-01 12:02:30 +0300

    Add JUnit5 tests

---
+++ src/test/kotlin/org/stepik/algorithms/lesson1/task1/BracketingInCodeKtTest.kt
@@ -0,0 +1,12 @@
+package org.stepik.algorithms.lesson1.task1
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class BracketingInCodeKtTest {
+
+    @Test
+    fun isBalanced() {
+    }
+}
